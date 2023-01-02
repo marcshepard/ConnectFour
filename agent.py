0690eb403eda0f58685b123a6a12b9e8695eb79b
marcshepard <marcshepard@outlook.com>
2023-01-02 13:33:20 -0800

    Factored rl_agent into it's own file and made some fixes after testing in the kaggle notebook

--- agent.py
+++ agent.py
@@ -168,12 +168,12 @@ def minimax3_agent(obs : SimpleNamespace, config : SimpleNamespace) -> int:
         return False
 
     # Minimax implementation
-    def minimax(node, depth, maximizingPlayer, mark, config):
+    def minimax(node, depth, maximizing, mark, config):
         is_terminal = is_terminal_node(node, config)
         valid_moves = [c for c in range(config.columns) if node[0][c] == 0]
         if depth == 0 or is_terminal:
             return get_heuristic(node, mark, config)
-        if maximizingPlayer:
+        if maximizing:
             value = -np.Inf
             for col in valid_moves:
                 child = drop_piece(node, col, mark, config)
@@ -198,120 +198,3 @@ def minimax3_agent(obs : SimpleNamespace, config : SimpleNamespace) -> int:
     max_cols = [move for move, score in scores.items() if score == max(scores.values())]
     # Select a column that maximizes the min possible score. Take the middle column if tied
     return max_cols[len(max_cols)//2]
-
-
-"""
-# The final agent has no preset rules - rather it uses reinforced learning after battling against the other agents
-# Requires pip install pandas, torch, gym, stable_baselines3 - which don't work on Python 3.11
-# TODO - this is skeletal code from the class; I've not yet tried and debugged it since I'm on Python 3.11 and haven't down-graded
-from sys import version_info
-def py_ver_ok():
-    return version_info[0] == 3 and 8 <= version_info[1] < 11
-
-model_data = "sb3_model_data"   # It takes forever to train an sb3 model, so need a place to save it when done
-
-if py_ver_ok():
-    import gym
-    from gym import spaces
-    import torch as th
-    import torch.nn as nn
-    from stable_baselines3 import PPO 
-    from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
-    import random
-
-    # To prep for ML training, we first need to create an environment compatible with Stable Baselines.
-    # For this, we define the ConnectFourGym class below. This class implements ConnectX as an OpenAI Gym environment
-    class ConnectFourGym(gym.Env):
-        def __init__(self, agent2="random"):
-            ks_env = make("connectx", debug=True)
-            self.env = ks_env.train([None, agent2])
-            self.rows = ks_env.configuration.rows
-            self.columns = ks_env.configuration.columns
-            # Learn about spaces here: http://gym.openai.com/docs/#spaces
-            self.action_space = spaces.Discrete(self.columns)
-            self.observation_space = spaces.Box(low=0, high=2, 
-                                                shape=(1,self.rows,self.columns), dtype=int)
-            # Tuple corresponding to the min and max possible rewards
-            self.reward_range = (-10, 1)
-            # StableBaselines throws error if these are not defined
-            self.spec = None
-            self.metadata = None
-        def reset(self):
-            self.obs = self.env.reset()
-            return np.array(self.obs['board']).reshape(1,self.rows,self.columns)
-        def change_reward(self, old_reward, done):
-            if old_reward == 1: # The agent won the game
-                return 1
-            elif done: # The opponent won the game
-                return -1
-            else: # Reward 1/42
-                return 1/(self.rows*self.columns)
-        def step(self, action):
-            # Check if agent's move is valid
-            is_valid = (self.obs['board'][int(action)] == 0)
-            if is_valid: # Play the move
-                self.obs, old_reward, done, _ = self.env.step(int(action))
-                reward = self.change_reward(old_reward, done)
-            else: # End the game and penalize agent
-                reward, done, _ = -10, True, {}
-            return np.array(self.obs['board']).reshape(1,self.rows,self.columns), reward, done, _
-
-    # Neural network for predicting action values
-    class CustomCNN(BaseFeaturesExtractor):
-        def __init__(self, observation_space: gym.spaces.Box, features_dim: int=128):
-            super(CustomCNN, self).__init__(observation_space, features_dim)
-            # CxHxW images (channels first)
-            n_input_channels = observation_space.shape[0]
-            self.cnn = nn.Sequential(
-                nn.Conv2d(n_input_channels, 32, kernel_size=3, stride=1, padding=0),
-                nn.ReLU(),
-                nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=0),
-                nn.ReLU(),
-                nn.Flatten(),
-            )
-
-            # Compute shape by doing one forward pass
-            with th.no_grad():
-                n_flatten = self.cnn(
-                    th.as_tensor(observation_space.sample()[None]).float()
-                ).shape[1]
-
-            self.linear = nn.Sequential(nn.Linear(n_flatten, features_dim), nn.ReLU())
-
-        def forward(self, observations: th.Tensor) -> th.Tensor:
-            return self.linear(self.cnn(observations))
-
-    # This method trains the rl_agent and persists the model to the given file_name so we can later load it
-    def train_rl_agent():  
-        # We'll train out reenforced learning agent by letting it play against the basic agent
-        env = ConnectFourGym(agent2=basic_agent)
-
-        policy_kwargs = dict(
-            features_extractor_class=CustomCNN,
-        )
-                
-        # Initialize agent
-        model = PPO("CnnPolicy", env, policy_kwargs=policy_kwargs, verbose=0)
-
-        # Train agent
-        model.learn(total_timesteps=60000)
-        model.save(model_data)
-        return True
-
-    rl_model = None
-
-    # Add interface for the trained agent
-    def rl_agent1(obs, config):
-        if rl_model is None:
-            env = ConnectFourGym()
-            rl_model = PPO.load (model_data, env=env)
-        # Use the best model to select a column
-        col, _ = rl_model.predict(np.array(obs['board']).reshape(1, 6,7))
-        # Check if selected column is valid
-        is_valid = (obs['board'][int(col)] == 0)
-        # If not valid, select random move. 
-        if is_valid:
-            return int(col)
-        else:
-            return random.choice([col for col in range(config.columns) if obs.board[int(col)] == 0])
-"""
